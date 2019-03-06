672b135b1ef2d832a6f24be4eba58a4f3970b38b
vedhavarshini <tmmarimuthu@gmail.com>
2019-03-06 14:33:45 +0530

    Create pyex01.py

---
+++ pyex01.py
@@ -0,0 +1,8 @@
+#This program displays where the entered number is positive or negative or zero
+num = int(raw_input())
+if num>0:
+	print("Positive")
+elif num <0:
+	print("Negative")
+else:
+	print("Zero")
